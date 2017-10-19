2cb623276bfd6b7b06b871f474ad563dabdd22c4
Antommyj <antommy1985@gmail.com>
2017-10-19 20:52:58 +0700

    This is a second update on the process

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
 // This module require the filesystem and module library
 var fs = require('fs');
-var moment = require('moment');
+var moment = require('moment'); //My favorite javascript date library
 
 var processObject = function(){
 
@@ -46,32 +46,97 @@ var processObject = function(){
     //This is another method to post all the data to console
     this.showData = function(){
       var data = this.returnExistingData();
-      data.forEach(function(item, index){
-          console.log(index+1 + ". Potentio: " + item.potentio +
-              ". Temperature: " + item.temp +
-              ". Humidity: " + item.humidity +
-              ". Time: " + item.time)
-      })
+      if(data.length === 0){
+          console.log("There is no data in here!")
+      }else{
+          data.forEach(function(item, index){
+              console.log(index+1 + ". Potentio: " + item.potentio +
+                  ". Temperature: " + item.temp +
+                  ". Humidity: " + item.humidity +
+                  ". Distance: " + item.distance +
+                  ". Time: " + item.time)
+          })
+      }
+
     };
 
     //This is another method to delete the data
     this.deleteData = function(){
         // Check whether the user input the data index
-        if(process.argv[3] === undefined){
-            console.log("Error: You need to type in the index. Command: Del indexNo")
+        if(process.argv[3] === undefined) {
+            console.log("Error: You need to type how many data to delete from beginning ('del all' to delete all)");
+        }else if(process.argv[3] === "all"){
+            process.stdout.write("Are you SURE you want to delete ALL the data stored? (Y/N)");
+            process.stdin.on("data", function(data){
+                var answer = data.toString().trim();
+                if(answer === "Y"){
+                    var dataToDelete = JSON.parse(fs.readFileSync("./data1.json"));
+                    var numberOfData = dataToDelete.length;
+                    dataToDelete.splice(0, numberOfData);
+                    fs.writeFile('./data1.json', JSON.stringify(dataToDelete),function(error){
+                        if(error){
+                            console.log("Fail delete!")
+                        }else{
+                            console.log("Delete Succesful!")
+                        }
+                    });
+                }else{
+                    process.exit();
+                }
+            })
         }else{
-
+            process.stdout.write("Are you sure you want to delete " + process.argv[3] + " earliest data? (Y/N)");
+            process.stdin.on('data', function(data){
+                var answer = data.toString().trim();
+                if(answer === "Y"){
+                    var dataToDelete = JSON.parse(fs.readFileSync("./data1.json"));
+                    dataToDelete.splice(0, process.argv[3]);
+                    fs.writeFile("./data1.json", JSON.stringify(dataToDelete) ,function(error){
+                        if(error){
+                            console.log("Error at writing to file");
+                        }else{
+                            console.log(process.argv[3] + " Data deleted");
+                        }
+                    });
+                }else{
+                    process.exit();
+                }
+            });
         }
     };
+
+
+    this.populate = function(){
+        var existingData = this.returnExistingData();
+        var dataNeeded = 10 - existingData.length;
+        for(var i = 0; i < dataNeeded; i++){
+            var someFnObject = {
+                potentio : Math.floor(Math.random() * 1024),
+                temp: Math.floor(Math.random() * 40) + 20,
+                humidity: Math.floor(Math.random() * 90) + 20,
+                distance: Math.floor(Math.random() * 50)+10,
+                time: moment().format("LTS")
+            };
+            existingData.push(someFnObject);
+        }
+        fs.writeFileSync("./data1.json", JSON.stringify(existingData));
+
+    };
+
+
 
     // This is the main method that assigned the data into an object and write it to "./data1.json"
     this.changeData = function(){
       if(this.command === "add" || this.command === "ADD" || this.command === "Add"){
           this.addData();
       }else if(this.command === "del" || this.command === "DEL"){
-          console.log("Deleting")
+          this.deleteData();
       }else if(this.command === "show"){
           this.showData();
+      }else if(this.command === "populate"){
+          this.populate();
+      }else{
+          console.log("Unknown Command")
       }
     };
 
@@ -81,6 +146,9 @@ var processObject = function(){
 var action = new processObject();
 // and execute command
 action.changeData();
+
+
+
 
 
 
